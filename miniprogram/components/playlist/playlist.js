567de541971a8ea9583a8a38e992f6e175e7b6f8
v_shtongliu <v_shtongliu@tencent.com>
2019-12-20 17:50:58 +0800

    playlist

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -8,18 +8,40 @@ Component({
             type: Object
         }
     },
+    // 监听器
+    observers: {
+        ['itemObj.playCount'](count) {
+            this.setData({
+                playCount: this.tranNum(count, 2)
+            })
+        }
+    },
 
     /**
      * 组件的初始数据
      */
     data: {
-
+        playCount: 0
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-
+        // 转化数字的方法
+        tranNum(count, num) {
+            let numStr = count.toString().split('.')[0]
+            if (numStr.length < 6) {
+                return numStr
+            } else if (numStr.length >= 6 && numStr.length <= 8) {
+                let decimal = numStr.substring(numStr.length - 4, numStr.length - 4 + num)
+                let lastNum = parseInt(count / 10000) + '.' + decimal
+                return parseFloat(lastNum) + '万'
+            } else if (numStr.length > 8) {
+                let decimal = numStr.substring(numStr.length - 8, numStr.length - 8 + num)
+                let lastNum = parseInt(count / 100000000) + '.' + decimal
+                return parseFloat(lastNum) + '亿'
+            }
+        }
     }
 })
